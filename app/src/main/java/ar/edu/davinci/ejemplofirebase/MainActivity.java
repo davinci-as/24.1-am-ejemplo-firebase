f90de9110d198b7c32470a83982fe9b292d579e6
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-05-22 20:27:16 -0300

    feat: get users from firebase

--- app/src/main/java/ar/edu/davinci/ejemplofirebase/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/ejemplofirebase/MainActivity.java
@@ -1,14 +1,33 @@
 package ar.edu.davinci.ejemplofirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d("TAG", document.getId() + " => " + document.getData());
+                    }
+                }
+            }
+        });
     }
 }
